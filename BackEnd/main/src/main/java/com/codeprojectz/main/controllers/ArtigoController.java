cbd13efedbf2231e134a1c1df261419cc31f5cb6
c-Alvinn <alvaro.rdias14@gmail.com>
2024-04-22 20:51:40 -0300

    Add Categoria Get Method and Artigo Post

--- BackEnd/main/src/main/java/com/codeprojectz/main/controllers/ArtigoController.java
+++ BackEnd/main/src/main/java/com/codeprojectz/main/controllers/ArtigoController.java
@@ -1,7 +1,17 @@
 package com.codeprojectz.main.controllers;
 
+import com.codeprojectz.main.dtos.ArtigoRecordDto;
+import com.codeprojectz.main.models.Artigo;
 import com.codeprojectz.main.repositories.ArtigoRepository;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +21,11 @@ public class ArtigoController {
 
     @Autowired
     ArtigoRepository artigoRepository;
+
+    @PostMapping
+    public ResponseEntity<Artigo> addArtigo(@RequestBody @Valid ArtigoRecordDto artigoRecordDto) {
+        var artigo = new Artigo();
+        BeanUtils.copyProperties(artigoRecordDto, artigo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(artigoRepository.save(artigo));
+    }
 }

a791d182a689ad10192c24f9dec1022f37172f74
c-Alvinn <alvaro.rdias14@gmail.com>
2024-05-07 19:55:51 -0300

    Fix CorsConfiguration

--- BackEnd/main/src/main/java/com/codeprojectz/main/CorsConfiguration.java
+++ BackEnd/main/src/main/java/com/codeprojectz/main/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:8437")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }

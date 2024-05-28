0873b71320f657834d9ab964b3a0c2ec79b84351
Augusto Castejon <75548446+Augusto-Castejon@users.noreply.github.com>
2024-05-27 21:10:16 -0300

    Create ProfileScreen

---
+++ FrontEnd/src/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ProfileScreen = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get('/api/user/profile'); // Supondo que esta seja a rota correta para obter o perfil do usuário
+        setUser(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erro ao obter perfil do usuário:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
+  return (
+    <div className="ProfileScreen-container">
+      <h1>Meu Perfil</h1>
+      <div className="user-profile">
+        <img src={user && user.photo}/>
+      </div>
+      <div className="profile-details">
+        <h2>Informações Gerais</h2>
+        <p><strong>Nome:</strong> {user && user.nome}</p>
+        <p><strong>Sobrenome:</strong> {user && user.sobrenome}</p>
+        <p><strong>Email:</strong> {user && user.email}</p>
+        <p><strong>Data de Nascimento:</strong> {user && user.dataNascimento}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileScreen;
+    

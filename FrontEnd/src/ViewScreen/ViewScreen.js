3ad51e747a77e6244810369b4948e8e3c6ad97f3
Alvin <141906500+c-Alvinn@users.noreply.github.com>
2024-06-03 20:16:10 -0300

    Update ViewScreen.js

--- FrontEnd/src/ViewScreen/ViewScreen.js
+++ FrontEnd/src/ViewScreen/ViewScreen.js
@@ -4,70 +4,67 @@ import ReactMarkdown from 'react-markdown';
 import './ViewScreen.css';
 import { useParams } from 'react-router-dom';
 
-function ViewScreen({ }) { // Utilizando match para capturar o parâmetro da rota
-    const { artigoID } = useParams();
-    console.log(artigoID);
-    const [articleData, setArticleData] = useState(null);
-    const [imageData, setImageData] = useState(null);
-    const [markdownString, setMarkdownString] = useState(null);
-    const [markdownData, setMarkdownData] = useState(null);
-    const [error, setError] = useState('');
+function ViewScreen({  }) { // Utilizando match para capturar o parâmetro da rota
+  const { artigoID } = useParams();
+  console.log(artigoID);
+  const [articleData, setArticleData] = useState(null);
+  const [imageData, setImageData] = useState(null);
+  const [markdownString, setMarkdownString] = useState(null);
+  const [markdownData, setMarkdownData] = useState(null);
+  const [error, setError] = useState('');
+  
+
+  useEffect(() => {
+      // Primeira chamada para buscar os dados do artigo
+      fetchArticleData();
+  }, []);
+
+  
+
+  const fetchArticleData = async () => {
+      try {
+          const response = await axios.get(`http://192.168.7.21:6419/artigo/id/${artigoID}`);
+          if (response.data) {
+              setArticleData(response.data);
+              fetchContentData(response.data.conteudo.conteudoID, setMarkdownData);
+              fetchContentData(response.data.imagem.conteudoID, setImageData);
+          }
+      } catch (error) {
+          setError('Erro ao buscar dados do artigo.');
+          console.error('Erro ao buscar o artigo:', error);
+      }
+  };
+
+  const fetchContentData = async (conteudoID, setter) => {
+      try {
+          const response = await axios.get(`http://192.168.7.21:6419/conteudo/id/${conteudoID}`, {
+              responseType: 'blob'  // Importante para arquivos binários como imagens
+          });
+          const url = URL.createObjectURL(response.data);
+          setter(url);
+      } catch (error) {
+          setError('Erro ao buscar conteúdo relacionado.');
+          console.error('Erro ao buscar conteúdo:', error);
+      }
+  };
+
+  fetch(markdownData).then(res => res.text()).then(text => {
+    setMarkdownString(text);
+  });
 
 
-    useEffect(() => {
-        // Primeira chamada para buscar os dados do artigo
-        fetchArticleData();
-    }, []);
-
-
-
-    const fetchArticleData = async () => {
-        console.log("Aquii");
-        try {
-            const response = await axios.get(`http://localhost:6419/artigo/id/${artigoID}`);
-            if (response.data) {
-                setArticleData(response.data);
-                fetchContentData(response.data.conteudo.conteudoID, setMarkdownData);
-                fetchContentData(response.data.imagem.conteudoID, setImageData);
-            }
-        } catch (error) {
-            setError('Erro ao buscar dados do artigo.');
-            console.error('Erro ao buscar o artigo:', error);
-        }
-    };
-
-    const fetchContentData = async (conteudoID, setter) => {
-        try {
-            console.log(articleData);
-            const response = await axios.get(`http://localhost:6419/conteudo/id/${conteudoID}`, {
-                responseType: 'blob'  // Importante para arquivos binários como imagens
-            });
-            const url = URL.createObjectURL(response.data);
-            setter(url);
-        } catch (error) {
-            setError('Erro ao buscar conteúdo relacionado.');
-            console.error('Erro ao buscar conteúdo:', error);
-        }
-    };
-
-    fetch(markdownData).then(res => res.text()).then(text => {
-        setMarkdownString(text);
-    });
-
-    console.log(markdownString);
-
-    if (error) return <div>{error}</div>;
+  if (!articleData) return <div>Carregando...</div>;
+  if (error) return <div>{error}</div>;
 
     return (
         <div className="article-screen">
             <div className="article-container">
                 <h1 className="article-title">{articleData.titulo}</h1>
                 <h2 className="article-instructor">Criado por: {articleData.criador.nome}</h2>
-                {imageData && <img src={imageData} alt="Conteudo visual do artigo" />}
+                {imageData && <img src={imageData} alt="Article visual content" />}
                 <p className="article-content">{articleData.descricao}</p>
-                <br></br>
-                <ReactMarkdown>{markdownString}</ReactMarkdown>
                 <h3 className="article-category">Categoria: {articleData.categoria.nome}</h3>
+                <ReactMarkdown>{markdownString}</ReactMarkdown>
             </div>
         </div>
     );

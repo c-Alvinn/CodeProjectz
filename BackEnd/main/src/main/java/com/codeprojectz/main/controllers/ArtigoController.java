64335f9355a485b0f7cbde41a12589bc72282fbb
Alvin <141906500+c-Alvinn@users.noreply.github.com>
2024-06-06 14:30:33 -0300

    Add DELETE Artigo

--- BackEnd/main/src/main/java/com/codeprojectz/main/controllers/ArtigoController.java
+++ BackEnd/main/src/main/java/com/codeprojectz/main/controllers/ArtigoController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -110,5 +111,15 @@ public class ArtigoController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(lista);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteArtigo(@PathVariable(value="id") Integer id){
+        Artigo artigo = artigoRepository.findById(id).get();
+        if (artigo == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        artigoRepository.delete(artigo);
+        return ResponseEntity.status(HttpStatus.OK).body("Artigo excluido com sucesso!");
     }
 }

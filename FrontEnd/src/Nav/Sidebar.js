e8a231e08af5f962a5631e1f854ada006f0de1ea
NahNathan <nathanrodriguessantos3@gmail.com>
2024-06-10 10:20:59 -0300

    Corrige Addscreen & Sidebar

--- FrontEnd/src/Nav/Sidebar.js
+++ FrontEnd/src/Nav/Sidebar.js
@@ -1,9 +1,7 @@
-// Sidebar.js
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './Sidebar.css';
 import userImage from '../assets/images/placeholder.png'; 
-
 
 function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,12 +9,11 @@ function Sidebar() {
     const location = useLocation(); 
 
     const toggleSidebar = () => {
-        setIsOpen(!isOpen);
+        setIsOpen(!isOpen);  // Inverte o estado da Sidebar
     };
 
     // Lista de caminhos onde a Sidebar não deve aparecer
     const hiddenPaths = ['/', '/signup'];
-    console.log(location)
 
     // Verifica se a rota atual está na lista de rotas onde a Sidebar não deve ser exibida
     if (hiddenPaths.includes(location.pathname)) {
@@ -29,7 +26,11 @@ function Sidebar() {
                 <img src={userImage} alt="User" />
             </div>
             <div className="sidebar-toggle" onClick={toggleSidebar}>
-                <div className="icon"></div>
+                {isOpen ? (
+                    <div className="icon">X</div>  // Ícone para o estado "aberto"
+                ) : (
+                    <div className="icon">&#9776;</div>  // Ícone tradicional de hambúrguer para o estado "fechado"
+                )}
             </div>
             <nav className='botoes-menu'>
                 <button type='button' onClick={() => { toggleSidebar(); navigate('/home'); }}>Artigos</button>

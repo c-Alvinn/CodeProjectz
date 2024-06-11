2e9584bfacc526e67199af3cca94e25c75663a2a
Nathan Rodrigues dos Santos <97409681+NahNathan@users.noreply.github.com>
2024-06-11 20:08:31 -0300

    Mostra artigos por categoria

--- FrontEnd/src/Home/HomePage.js
+++ FrontEnd/src/Home/HomePage.js
@@ -8,22 +8,19 @@ function HomePage() {
     const [artigos, setArtigos] = useState([]);
     const [categorias, setCategorias] = useState([]);
 
-
-    useEffect(() => {    
-            fetchArtigos();
-            fetchCategorias()
+    useEffect(() => {
+        fetchArtigos();
+        fetchCategorias();
     }, []);
 
     const fetchArtigos = async () => {
         try {
             const response = await axios.get('http://localhost:6419/artigo/lastFive');
             if (response.status === 200) {
-                console.log(response.data);
                 const artigosComImagens = await Promise.all(response.data.map(async (artigo) => {
                     return await fetchArtigoCompleto(artigo);
                 }));
                 setArtigos(artigosComImagens);
-                console.log(artigosComImagens);
             }
         } catch (error) {
             console.error('Erro ao buscar artigos:', error);
@@ -32,8 +29,6 @@ function HomePage() {
 
     const fetchArtigoCompleto = async (artigo) => {
         try {
-            // Assume que o ID da imagem está armazenado no campo imagemID do artigo
-            console.log(artigo.imagem.conteudoID);
             const resImagem = await axios.get(`http://localhost:6419/conteudo/id/${artigo.imagem.conteudoID}`, { responseType: 'blob' });
             const urlImagem = URL.createObjectURL(resImagem.data);
             return {
@@ -49,8 +44,6 @@ function HomePage() {
         }
     };
 
-
-    //Pegando todas as categorias:
     const fetchCategorias = async () => {
         try {
             const response = await axios.get('http://localhost:6419/categoria');
@@ -62,87 +55,84 @@ function HomePage() {
         }
     };
 
+
     function FetchLastFiveArticles({ categoryId }) {
-        
+        const [artigosCategoria, setArtigosCategoria] = useState([]);
     
         useEffect(() => {
+            const fetchLastFiveArticles = async () => {
+                try {
+                    const response = await axios.get(`http://localhost:6419/artigo/lastFive/${categoryId}`);
+                    if (response.status === 200) {
+                        const artigosComImagens = await Promise.all(response.data.map(async (artigo) => {
+                            const urlImagem = await fetchArtigoImageURL(artigo.imagem.conteudoID);
+                            return { ...artigo, imagemURL: urlImagem };
+                        }));
+                        setArtigosCategoria(artigosComImagens);
+                    }
+                } catch (error) {
+                    console.error('Erro ao buscar artigos da categoria:', error);
+                }
+            };
+    
             fetchLastFiveArticles();
         }, [categoryId]);
-
-        const fetchLastFiveArticles = async () => {
+    
+        const fetchArtigoImageURL = async (conteudoID) => {
             try {
-                const response = await axios.get(`http://localhost:6419/artigo/lastFive/${categoryId}`);
-                if (response.status === 200) {
-                    const artigosComImagens = await Promise.all(response.data.map(async (artigo) => {
-                        return await fetchArtigoCompleto(artigo);
-                    }));
-                    setArtigos(artigosComImagens);
-                }
-            } catch (error) {
-                console.error('Erro ao buscar artigos da categoria:', error);
-            }
-        };
-
-        const fetchArtigoCompleto = async (artigo) => {
-            try {
-                const resImagem = await axios.get(`http://localhost:6419/conteudo/id/${artigo.imagem.conteudoID}`, { responseType: 'blob' });
-                const urlImagem = URL.createObjectURL(resImagem.data);
-                return {
-                    ...artigo,
-                    imagemURL: urlImagem
-                };
+                const resImagem = await axios.get(`http://localhost:6419/conteudo/id/${conteudoID}`, { responseType: 'blob' });
+                return URL.createObjectURL(resImagem.data);
             } catch (error) {
                 console.error('Erro ao buscar imagem do artigo:', error);
-                return {
-                    ...artigo,
-                    imagemURL: '' // Ou algum placeholder padrão
-                };
+                return ''; // Ou algum placeholder padrão
             }
         };
-
+    
         return (
             <>
-                {artigos.map((artigo, index) => (
-                    <Card
-                        key={index}
-                        title={artigo.titulo}
-                        description={artigo.descricao}
-                        category={artigo.categoria.nome}
-                        image={artigo.imagemURL}
-                    />
+                {artigosCategoria.map((artigo, index) => (
+                    <Link key={index} to={`/view/${artigo.artigoID}`}>
+                        <Card
+                            title={artigo.titulo}
+                            description={artigo.descricao}
+                            category={artigo.categoria.nome}
+                            image={artigo.imagemURL}
+                        />
+                    </Link>
                 ))}
             </>
-        );}
-
-        return (
-            <div className="home-page">
-                <h2>Últimos Artigos</h2>
-                <div className="scroll-container">
-                    {/* Renderiza os últimos artigos */}
-                    {artigos.map((artigo, index) => (
-                        <Link key={index} to={`/view/${artigo.artigoID}`}>
-                            <Card
-                                title={artigo.titulo}
-                                description={artigo.descricao}
-                                category={artigo.categoria.nome}
-                                image={artigo.imagemURL}
-                            />
-                        </Link>
-                    ))}
-                </div>
+        );
+    }
     
-                {/* Renderiza um scroll-container para cada categoria */}
-                {categorias.map((categoria, index) => (
-                    <div key={index}>
-                        <h2>{categoria.nome}</h2>
-                        <div className="scroll-container">
-                            {/* Componente para buscar os últimos 5 artigos de uma categoria */}
-                            <FetchLastFiveArticles categoryId={categoria.id} />
-                        </div>
-                    </div>
+
+    return (
+        <div className="home-page">
+            <h2>Últimos Artigos</h2>
+            <div className="scroll-container">
+                {artigos.map((artigo, index) => (
+                    <Link key={index} to={`/view/${artigo.artigoID}`}>
+                        <Card
+                            title={artigo.titulo}
+                            description={artigo.descricao}
+                            category={artigo.categoria.nome}
+                            image={artigo.imagemURL}
+                        />
+                    </Link>
                 ))}
             </div>
-        );
+            {categorias.map((categoria, index) => (
+                <div key={index}>
+                    <h2>{categoria.nome}</h2>
+                    
+                    <div className="scroll-container">
+                        <FetchLastFiveArticles categoryId={categoria.categoriaID} />
+                    </div>
+                </div>
+            ))}
+
+
+        </div>
+    );
 }
 
 export default HomePage;

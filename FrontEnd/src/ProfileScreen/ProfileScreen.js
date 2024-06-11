89f585d17a6c43c096529a74c744b48a1b92787d
Augusto Castejon <75548446+Augusto-Castejon@users.noreply.github.com>
2024-06-11 20:22:17 -0300

    Profile up

--- FrontEnd/src/ProfileScreen/ProfileScreen.js
+++ FrontEnd/src/ProfileScreen/ProfileScreen.js
@@ -15,7 +15,7 @@ const formatDate = (dateString) => {
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const response = await axios.get('http://localhost:6419/usuario/perfil/1'); 
+        const response = await axios.get(`http://localhost:6419/usuario/perfil/${userID}`); 
         setUser(response.data);
         setLoading(false);
       } catch (error) {

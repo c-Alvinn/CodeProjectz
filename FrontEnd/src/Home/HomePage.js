739227ebbfb7664b028c8c080252f278831dc446
Nathan Rodrigues dos Santos <97409681+NahNathan@users.noreply.github.com>
2024-06-10 21:55:16 -0300

    Semi Complete Homepage fetching

--- FrontEnd/src/Home/HomePage.js
+++ FrontEnd/src/Home/HomePage.js
@@ -6,94 +6,17 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
     const [artigos, setArtigos] = useState([]);
-    const useMockData = true; // Altere isso para false para usar dados reais
-    const artigosMockados = [
-        {
-            artigoID: 1,
-            titulo: "Aprenda React Hoje",
-            descricao: "Um guia completo para aprender React e construir aplicações poderosas.",
-            categoria: { nome: "Desenvolvimento Web" },
-            imagem: {
-                conteudoID: '1'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-        {
-            artigoID: 2,
-            titulo: "Explorando Node.js",
-            descricao: "Descubra como Node.js pode acelerar o desenvolvimento do lado do servidor.",
-            categoria: { nome: "Backend" },
-            imagem: {
-                conteudoID: '2'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-        {
-            artigoID: 3,
-            titulo: "Explorando Node.js",
-            descricao: "Descubra como Node.js pode acelerar o desenvolvimento do lado do servidor.",
-            categoria: { nome: "Backend" },
-            imagem: {
-                conteudoID: '2'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-        {
-            artigoID: 4,
-            titulo: "Explorando Node.js",
-            descricao: "Descubra como Node.js pode acelerar o desenvolvimento do lado do servidor.",
-            categoria: { nome: "Backend" },
-            imagem: {
-                conteudoID: '2'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-        {
-            artigoID: 5,
-            titulo: "Explorando Node.js",
-            descricao: "Descubra como Node.js pode acelerar o desenvolvimento do lado do servidor.",
-            categoria: { nome: "Backend" },
-            imagem: {
-                conteudoID: '2'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-        {
-            artigoID: 4,
-            titulo: "Explorando Node.js",
-            descricao: "Descubra como Node.js pode acelerar o desenvolvimento do lado do servidor.",
-            categoria: { nome: "Backend" },
-            imagem: {
-                conteudoID: '2'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-        {
-            artigoID: 4,
-            titulo: "Explorando Node.js",
-            descricao: "Descubra como Node.js pode acelerar o desenvolvimento do lado do servidor.",
-            categoria: { nome: "Backend" },
-            imagem: {
-                conteudoID: '2'
-            },
-            imagemURL: "https://via.placeholder.com/150"
-        },
-
-        // Adicione mais artigos conforme necessário
-    ];
+    const [categorias, setCategorias] = useState([]);
 
 
-    useEffect(() => {
-        if (useMockData) {
-            setArtigos(artigosMockados); //Vou tirar isso aqui depois, usar apenas para teste
-        } else {
+    useEffect(() => {    
             fetchArtigos();
-        }
+            fetchCategorias()
     }, []);
 
     const fetchArtigos = async () => {
         try {
-            const response = await axios.get('http://localhost:6419/artigo');
+            const response = await axios.get('http://localhost:6419/artigo/lastFive');
             if (response.status === 200) {
                 console.log(response.data);
                 const artigosComImagens = await Promise.all(response.data.map(async (artigo) => {
@@ -126,26 +49,100 @@ function HomePage() {
         }
     };
 
-    return (
-        <div className="home-page">
 
-            <h2>Últimos Artigos</h2>
-            <div className="scroll-container">
+    //Pegando todas as categorias:
+    const fetchCategorias = async () => {
+        try {
+            const response = await axios.get('http://localhost:6419/categoria');
+            if (response.status === 200) {
+                setCategorias(response.data);
+            }
+        } catch (error) {
+            console.error('Erro ao buscar categorias:', error);
+        }
+    };
 
+    function FetchLastFiveArticles({ categoryId }) {
+        
+    
+        useEffect(() => {
+            fetchLastFiveArticles();
+        }, [categoryId]);
+
+        const fetchLastFiveArticles = async () => {
+            try {
+                const response = await axios.get(`http://localhost:6419/artigo/lastFive/${categoryId}`);
+                if (response.status === 200) {
+                    const artigosComImagens = await Promise.all(response.data.map(async (artigo) => {
+                        return await fetchArtigoCompleto(artigo);
+                    }));
+                    setArtigos(artigosComImagens);
+                }
+            } catch (error) {
+                console.error('Erro ao buscar artigos da categoria:', error);
+            }
+        };
+
+        const fetchArtigoCompleto = async (artigo) => {
+            try {
+                const resImagem = await axios.get(`http://localhost:6419/conteudo/id/${artigo.imagem.conteudoID}`, { responseType: 'blob' });
+                const urlImagem = URL.createObjectURL(resImagem.data);
+                return {
+                    ...artigo,
+                    imagemURL: urlImagem
+                };
+            } catch (error) {
+                console.error('Erro ao buscar imagem do artigo:', error);
+                return {
+                    ...artigo,
+                    imagemURL: '' // Ou algum placeholder padrão
+                };
+            }
+        };
+
+        return (
+            <>
                 {artigos.map((artigo, index) => (
-                    <Link key={index} to={`/view/${artigo.artigoID}`}>
-                        <Card
-                            title={artigo.titulo}
-                            description={artigo.descricao}
-                            category={artigo.categoria.nome}
-                            image={artigo.imagemURL}
-                        />
-                    </Link>
+                    <Card
+                        key={index}
+                        title={artigo.titulo}
+                        description={artigo.descricao}
+                        category={artigo.categoria.nome}
+                        image={artigo.imagemURL}
+                    />
+                ))}
+            </>
+        );}
+
+        return (
+            <div className="home-page">
+                <h2>Últimos Artigos</h2>
+                <div className="scroll-container">
+                    {/* Renderiza os últimos artigos */}
+                    {artigos.map((artigo, index) => (
+                        <Link key={index} to={`/view/${artigo.artigoID}`}>
+                            <Card
+                                title={artigo.titulo}
+                                description={artigo.descricao}
+                                category={artigo.categoria.nome}
+                                image={artigo.imagemURL}
+                            />
+                        </Link>
+                    ))}
+                </div>
+    
+                {/* Renderiza um scroll-container para cada categoria */}
+                {categorias.map((categoria, index) => (
+                    <div key={index}>
+                        <h2>{categoria.nome}</h2>
+                        <div className="scroll-container">
+                            {/* Componente para buscar os últimos 5 artigos de uma categoria */}
+                            <FetchLastFiveArticles categoryId={categoria.id} />
+                        </div>
+                    </div>
                 ))}
             </div>
-            
-        </div>
-    );
+        );
 }
 
 export default HomePage;
